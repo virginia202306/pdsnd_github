7f3bbcd430d57a2675666dd40682ae90180671e8
Virginia Lanz <virginia.lanz@swisslife-am.com>
2023-07-10 20:44:09 +0200

    Style: Formating change in bikeshare_4.py
    Small change on line 4 in the bikeshare_4.py file.

--- bikeshare_4.py
+++ bikeshare_4.py
@@ -4,6 +4,7 @@ import time
 import pandas as pd
 import numpy as np
 
+#Please check that the data for the three cities are available.
 CITY_DATA = {'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv'}
